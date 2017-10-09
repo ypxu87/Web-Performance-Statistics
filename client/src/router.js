22b49d4cc6100d82f3e81ad24f7bff7beee42a6b
ypxu87 <ypxu87@163.com>
2017-10-09 16:47:06 +0800

    change roadhog to webpack

--- client/src/router.js
+++ client/src/router.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { Router, Route, Switch } from 'dva/router';
+import { Router, Route } from 'dva/router';
 import Welcome from './routes/welcome';
 import Home from './routes/home';
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
-      <Switch>
-        <Route path="/welcome" exact component={Welcome} />
-        <Route path="/" exact component={Home} />
-      </Switch>
+      <Route path="/welcome" exact component={Welcome} />
+      <Route path="/" exact component={Home} />
     </Router>
   );
 }
--- client/src/routes/home.js
+++ client/src/routes/home.js
@@ -18,11 +18,11 @@ class Home extends React.Component {
   render() {
     return (
       <div className={styles.fullHeight}>
-        <header className={styles.header}>
+        <header className={styles.headerView}>
           <span className={styles.logo}>webux</span>
           <nav>
-            <span className={this.state.currentItem === 'home' ? styles.headerActivity : ''} refs="homeItem" onClick={this.setActivityItem.bind(this, 'home')}>主页</span>
-            <span className={this.state.currentItem === 'statistics' ? styles.headerActivity : ''} refs="homeItem" onClick={this.setActivityItem.bind(this, 'statistics')}>统计</span>
+            <span className={this.state.currentItem === 'home' ? styles.headerActivity : ''} onClick={this.setActivityItem.bind(this, 'home')}>主页</span>
+            <span className={this.state.currentItem === 'statistics' ? styles.headerActivity : ''} onClick={this.setActivityItem.bind(this, 'statistics')}>统计</span>
             <div className={styles.logout}>logout</div>
           </nav>
         </header>
